ec1e39e1d804a042760d176f78f9795ba33fb5e5
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-09-01 12:11:27 +0200

    reduce cyclomatic complexity

---
+++ R/no_extremes.R
@@ -0,0 +1,44 @@
+#' @keywords internal
+#' @importFrom ggplot2 geom_vline ggtitle
+#' @importFrom stats as.formula binomial coef glm
+#' @importFrom utils flush.console
+no_extremes <- function(
+  power_summary,
+  no_small,
+  p,
+  opti,
+  design,
+  design_digits
+) {
+  if (abs(min(power_summary$estimated_power) - no_small) < 1e-9) {
+    # all estimates are either 1 or 0, so we cannot determine the direction of
+    # change based on the model, so we randomly choose to increase or decrease
+    # the design parameter
+    decrease <- sample(c(TRUE, FALSE), 1)
+  } else {
+    sprintf("cbind(signif, non_signif) ~ %s", opti) |>
+      as.formula() |>
+      glm(
+        data = power_summary,
+        family = binomial
+      ) -> power_model
+    decrease <- xor(no_small, coef(power_model)[2] < 0)
+  }
+  power_summary[, opti] |>
+    abs() |>
+    min() -> current_min
+  power_summary[, opti] |>
+    abs() |>
+    max() -> current_max
+  ifelse(decrease, current_min / 2, current_max * 2) |>
+    round(digits = design_digits[opti]) |>
+    max(10^-design_digits[opti]) -> new_design
+  new_design * sign(design[[opti]]) -> new_design
+  # add vertical line with the estimate to plot and print
+  p <- p +
+    geom_vline(xintercept = new_design, colour = "blue", linewidth = 1) +
+    ggtitle(sprintf("next try: %s = %s", opti, as.character(new_design)))
+  print(p)
+  flush.console()
+  return(new_design)
+}
--- R/sample_new_design.R
+++ R/sample_new_design.R
@@ -14,17 +14,10 @@
 #' @return Numeric. The next design parameter value to test, or empty vector if
 #'   converged to target power.
 #'
-#' @importFrom dplyr across bind_rows filter first lag last lead left_join
-#' @importFrom dplyr mutate select slice_sample
-#' @importFrom ggplot2 aes geom_blank geom_errorbar geom_hline geom_line
-#' @importFrom ggplot2 geom_point geom_rect geom_ribbon geom_vline ggplot
-#' @importFrom ggplot2 ggtitle scale_x_continuous scale_y_continuous
-#' @importFrom mgcv gam
+#' @importFrom ggplot2 aes geom_blank geom_errorbar geom_hline  geom_point
+#' @importFrom ggplot2 ggplot scale_y_continuous
 #' @importFrom scales percent
 #' @importFrom rlang .data sym !!
-#' @importFrom stats as.formula binomial coef glm plogis predict qnorm
-#' @importFrom tidyr replace_na
-#' @importFrom utils head flush.console tail
 #' @keywords internal
 #' @noRd
 sample_new_design <- function(
@@ -73,174 +66,36 @@ sample_new_design <- function(
   # smallest design parameter value
   no_small <- 0.5 < min(power_summary$ucl)
   no_large <- max(power_summary$lcl) < power
-  if (no_small || no_large) {
-    # all estimates are either 1 or 0, so we cannot determine the direction of
-    # change based on the model, so we randomly choose to increase or decrease
-    # the design parameter
-    if (abs(min(power_summary$estimated_power) - no_small) < 1e-9) {
-      decrease <- sample(c(TRUE, FALSE), 1)
-    } else {
-      sprintf("cbind(signif, non_signif) ~ %s", opti) |>
-        as.formula() |>
-        glm(
-          data = power_summary,
-          family = binomial
-        ) -> power_model
-      decrease <- xor(no_small, coef(power_model)[2] < 0)
-    }
-    power_summary[, opti] |>
-      abs() |>
-      min() -> current_min
-    power_summary[, opti] |>
-      abs() |>
-      max() -> current_max
-    ifelse(decrease, current_min / 2, current_max * 2) |>
-      round(digits = design_digits[opti]) |>
-      max(10^-design_digits[opti]) -> new_design
-    new_design * sign(design[[opti]]) -> new_design
-    # add vertical line with the estimate to plot and print
-    p <- p +
-      geom_vline(xintercept = new_design, colour = "blue", linewidth = 1) +
-      ggtitle(sprintf("next try: %s = %s", opti, as.character(new_design)))
-    print(p)
-    flush.console()
-    return(new_design)
-  }
-  # in the corner case that we only have two design parameter values, one low
-  # and one high, we cannot fit a model, so we just return the average of the
-  # two design parameter values that are below and above the target power,
-  # respectively
-  if (nrow(power_summary) == 2) {
-    c(
-      power_summary[power_summary$ucl < power, opti],
-      power_summary[power_summary$lcl > power, opti]
-    ) |>
-      mean() |>
-      round(digits = design_digits[opti]) -> new_design
-    p <- p +
-      geom_vline(xintercept = new_design, colour = "blue", linewidth = 1) +
-      ggtitle(sprintf("next try: %s = %s", opti, as.character(new_design)))
-    print(p)
-    flush.console()
-    return(new_design)
-  }
-  power_summary <- preprare_model_data(power_summary)
-  try(
-    sprintf("cbind(signif, non_signif) ~ s(%s, bs = \"cs\", k = 3)", opti) |>
-      as.formula() |>
-      gam(data = power_summary, family = binomial()),
-    silent = TRUE
-  ) -> power_model
-  if (inherits(power_model, "try-error")) {
-    sprintf("cbind(signif, non_signif) ~ s(%s, bs = \"cs\", k = 4)", opti) |>
-      as.formula() |>
-      gam(data = power_summary, family = binomial()) -> power_model
-  }
-  data.frame(
-    x = seq(
-      pmax(min(abs(power_summary[, opti])), 10^(-design_digits[[opti]])),
-      max(abs(power_summary[, opti])) * 1.05,
-      by = 10^(-design_digits[[opti]])
-    ) *
-      sign(design[[opti]])
-  ) |>
-    `colnames<-`(opti) |>
-    left_join(
-      power_summary |>
-        select(!!opti, "n_sim", lower = "lcl", upper = "ucl"),
-      by = opti
-    ) |>
-    mutate(n_sim = replace_na(.data$n_sim, 0)) -> predict_data
-  prediction <- predict(
-    object = power_model,
-    newdata = predict_data,
-    se.fit = TRUE
+  situation <- ifelse(
+    no_small || no_large,
+    "no_extremes",
+    ifelse(nrow(power_summary) == 2, "midpoint", "model")
   )
-  predict_data |>
-    mutate(
-      fit = qnorm(0.5, prediction$fit, prediction$se.fit),
-      lcl = qnorm(0.025, prediction$fit, prediction$se.fit),
-      ucl = qnorm(0.975, prediction$fit, prediction$se.fit),
-      across(c("fit", "lcl", "ucl"), plogis)
-    ) -> predict_data
-  predict_data |>
-    filter(
-      .data$lower < power,
-      power < .data$upper,
-      .data$n_sim < max_sample
-    ) |>
-    bind_rows(
-      predict_data |>
-        filter(
-          lag(.data$lcl, 1, first(.data$lcl)) < power,
-          lead(.data$ucl, 1, last(.data$ucl)) >= power,
-          .data$n_sim < max_sample
-        )
-    ) -> candidate
-  while (nrow(candidate) >= 50) {
-    candidate |>
-      mutate(
-        subset = as.character(!!sym(opti)) |>
-          nchar()
-      ) |>
-      filter(.data$subset < max(.data$subset)) -> candidate
-  }
-  candidate |>
-    slice_sample(n = 1, weight_by = max_sample - .data$n_sim) -> new_design
-  new_design <- unlist(new_design[[opti]])
-  if (head(predict_data$fit, 1) < tail(predict_data$fit, 1)) {
-    sign(design[[opti]]) *
-      c(
-        min(abs(predict_data[power < predict_data$lcl, opti])),
-        min(abs(predict_data[power < predict_data$ucl, opti]))
-      ) |>
-        range() -> attr(new_design, "range")
-  } else {
-    sign(design[[opti]]) *
-      c(
-        max(abs(predict_data[power < predict_data$lcl, opti])),
-        max(abs(predict_data[power < predict_data$ucl, opti]))
-      ) |>
-        range() -> attr(new_design, "range")
-  }
-  attr(new_design, "estimate") <- predict_data[
-    which.min((predict_data$fit - power)^2),
-    opti
-  ]
-  p <- p +
-    geom_ribbon(
-      data = predict_data,
-      aes(ymin = .data$lcl, ymax = .data$ucl),
-      alpha = 0.1
-    ) +
-    geom_line(data = predict_data, aes(y = .data$fit)) +
-    geom_rect(
-      xmin = attr(new_design, "range")[1],
-      xmax = attr(new_design, "range")[2],
-      ymin = -Inf,
-      ymax = Inf,
-      alpha = 0.05,
-      colour = NA,
-      fill = "darkgreen"
-    ) +
-    geom_vline(
-      xintercept = attr(new_design, "estimate"),
-      colour = "darkgreen"
-    ) +
-    geom_vline(xintercept = new_design, colour = "blue", linewidth = 1) +
-    ggtitle(
-      sprintf(
-        "current estimate: %s = %s (%s; %s); next try: %s = %s",
-        opti,
-        as.character(attr(new_design, "estimate")),
-        as.character(attr(new_design, "range")[1]),
-        as.character(attr(new_design, "range")[2]),
-        opti,
-        as.character(new_design)
-      )
-    ) +
-    scale_x_continuous(limits = range(c(0, predict_data[[opti]])))
-  print(p)
-  flush.console()
-  return(new_design)
+  switch(
+    situation,
+    "no_extremes" = no_extremes(
+      p = p,
+      power_summary = power_summary,
+      no_small = no_small,
+      opti = opti,
+      design = design,
+      design_digits = design_digits
+    ),
+    "midpoint" = midpoint(
+      p = p,
+      power_summary = power_summary,
+      power = power,
+      design_digits = design_digits,
+      opti = opti
+    ),
+    "model" = opti_model(
+      p = p,
+      power_summary = power_summary,
+      power = power,
+      design = design,
+      design_digits = design_digits,
+      opti = opti,
+      max_sample = max_sample
+    )
+  )
 }
